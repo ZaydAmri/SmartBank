389685c8fa8518332adf2dab0c82841a24998d5c
Oueslati Ahmed Khalil <45701691+kamatchokhalil@users.noreply.github.com>
2022-06-23 14:37:58 +0100

    entities (offre,banque et agence)

---
+++ src/main/java/Enteties/Agence.java
@@ -0,0 +1,57 @@
+package Enteties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "agence")
+public class Agence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String adresse;
+    private int nbrInscrits;
+
+    public Agence() {
+    }
+
+    public Agence(int id, String name, String adresse, int nbrInscrits) {
+        this.id = id;
+        this.name = name;
+        this.adresse = adresse;
+        this.nbrInscrits = nbrInscrits;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getNbrInscrits() {
+        return nbrInscrits;
+    }
+
+    public void setNbrInscrits(int nbrInscrits) {
+        this.nbrInscrits = nbrInscrits;
+    }
+}
