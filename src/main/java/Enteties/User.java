02c07a19f2805a2aff4006caf6147920281dbe53
AmineMissaoui <amine.missaoui@esprit.tn>
2022-06-23 14:43:56 +0100

    add anotation

--- src/main/java/Enteties/User.java
+++ src/main/java/Enteties/User.java
@@ -1,6 +1,17 @@
 package Enteties;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public int id;
     public String ipAdress;
     public String nom;
