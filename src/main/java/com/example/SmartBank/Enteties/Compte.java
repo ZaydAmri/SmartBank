06ffb36babc5359d0fcb074bb6958f58983abe7b
Oueslati Ahmed Khalil <45701691+kamatchokhalil@users.noreply.github.com>
2022-06-29 15:53:27 +0100

    compte entity

--- src/main/java/com/example/SmartBank/Enteties/Compte.java
+++ src/main/java/com/example/SmartBank/Enteties/Compte.java
@@ -1,4 +1,26 @@
 package com.example.SmartBank.Enteties;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "compte")
 public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String state;
+    private double solde;
+    @ManyToOne
+    @JoinColumn(name = "intitule_id")
+    private Client intitule;
+    private String nomSurCarte;
+    private TypeCompte typeCompte;
+    private String sourceRevenus;
+
+    public Client getIntitule() {
+        return intitule;
+    }
+
+
 }
